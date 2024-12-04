970a457aebe3cee3533d6f5c27bfea5122956598
rankint1044 <rankint1044@student.faytechcc.edu>
2024-12-04 10:38:16 -0500

    Add files via upload

--- P1LAB1_RankinThomas.py
+++ P1LAB1_RankinThomas.py
@@ -1,4 +1,10 @@
- # Thomas Rankin
- # November 12, 2024
- # P1LAB1
- # Using IDLE in Python
+# Thomas Rankin
+# November 12, 2024
+# P1LAB1
+# Using IDLE in Python
+# Prompt the user for their first and last name
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Display a personalized welcome message
+print(f"Hello, {first_name} {last_name}! Welcome to CTI-110")
